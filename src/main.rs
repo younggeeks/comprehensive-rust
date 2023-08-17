e1a28f8b7b5090eb4ec2df752886c9cd9ebfd67d
Samwel Charles <younggeeks101@gmail.com>
2023-08-17 11:59:32 +0300

    Structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
 fn main() {
-    println!("Hello, world!");
+    let sam = Person::new(String::from("Samwel Charles"), 33);
+
+    println!("{sam:?}");
+
+    create_default()
+}
+
+fn create_default() {
+    let tmp = Person {
+        age: 33,
+        ..Default::default()
+    };
+
+    println!("{tmp:?}")
+}
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
+impl Person {
+    fn new(name: String, age: u8) -> Self {
+        Person { name, age }
+    }
+}
+
+impl Default for Person {
+    fn default() -> Self {
+        Self {
+            name: "user".to_string(),
+            age: 0,
+        }
+    }
 }
