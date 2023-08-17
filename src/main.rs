e0bb3844fd3b5b089bf4653807ef7bdc830afe6e
Samwel Charles <younggeeks101@gmail.com>
2023-08-17 12:04:49 +0300

    Simple struct

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,22 @@
 fn main() {
-    let sam = Person::new(String::from("Samwel Charles"), 33);
-
-    println!("{sam:?}");
-
-    create_default()
+    println!("You got: {:?}", flip_coin())
 }
 
-fn create_default() {
-    let tmp = Person {
-        age: 33,
-        ..Default::default()
-    };
-
-    println!("{tmp:?}")
+fn generate_random_number() -> i32 {
+    4
 }
+
 #[derive(Debug)]
-struct Person {
-    name: String,
-    age: u8,
+enum CoinFlip {
+    Heads,
+    Tails,
 }
 
-impl Person {
-    fn new(name: String, age: u8) -> Self {
-        Person { name, age }
-    }
-}
-
-impl Default for Person {
-    fn default() -> Self {
-        Self {
-            name: "user".to_string(),
-            age: 0,
-        }
+fn flip_coin() -> CoinFlip {
+    let random_number = generate_random_number();
+    if random_number % 2 == 0 {
+        CoinFlip::Heads
+    } else {
+        CoinFlip::Tails
     }
 }
